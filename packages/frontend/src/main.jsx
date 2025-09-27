d8951478d099c11ca3a7a33c40526b21957a0f15
qjalti <stig.guriev@gmail.com>
2025-09-27 12:55:18 +0300

    Добавление тёмной темы, отключение Electron

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -1,16 +1,12 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
-import {ThemeProvider} from '@mui/material/styles'
 import './index.css'
 import App from './App.jsx'
-import theme from './theme'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <App/>
-    </ThemeProvider>
+    <CssBaseline/>
+    <App/>
   </StrictMode>,
 )
--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,5 +1,5 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +12,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-});
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
+})
