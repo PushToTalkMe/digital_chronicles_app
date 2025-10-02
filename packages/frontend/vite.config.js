5bc28d19e758d50b44105598077bbd3d04917ac4
root <root@raggedstream.aeza.network>
2025-10-02 16:25:38 +0000

    fix: base url

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 3001,
